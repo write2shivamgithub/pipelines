cd0d3218c8895fbf3b0de17046ac62c7093290c4
write2shivamgithub <shivamshankaraacharya@gmail.com>
2024-01-01 17:01:31 +0530

    dvclive and mlflow code commit

--- train.py
+++ train.py
@@ -1,15 +1,29 @@
 import random
-import sys
-import yaml
 from dvclive import Live
+import yaml
+import mlflow
 
 with Live(save_dvc_exp=True) as live:
-    train_params = yaml.safe_load(open('params.yaml'))['train']
-    epochs = train_params['epochs']
-    live.log_param("epochs", epochs)
-    for epoch in range(epochs):
-        live.log_metric("train/accuracy", epoch + random.random())
-        live.log_metric("train/loss", epochs - epoch - random.random())
-        live.log_metric("val/accuracy",epoch + random.random() )
-        live.log_metric("val/loss", epochs - epoch - random.random())
-        live.next_step()
+    with mlflow.start_run():
+        epochs = yaml.safe_load(open('params.yaml'))['train']['epochs']
+
+        live.log_param("epochs", epochs)
+        mlflow.log_param("epochs",epochs)
+
+        for epoch in range(epochs):
+            train_acc = epoch + random.random()
+            train_loss = epochs - epoch - random.random()
+            val_acc = epoch + random.random()
+            val_loss = epochs - epoch - random.random()
+
+            live.log_metric("train/accuracy", train_acc)
+            live.log_metric("train/loss", train_loss)
+            live.log_metric("val/accuracy", val_acc )
+            live.log_metric("val/loss", val_loss)
+
+            mlflow.live.log_metric("train/accuracy", train_acc)
+            mlflow.live.log_metric("train/loss", train_loss)
+            mlflow.live.log_metric("val/accuracy",val_acc)
+            mlflow.live.log_metric("val/loss", val_loss)
+
+            live.next_step()
